9300d73a2259034f56a44e5f678b4af3e0818873
Alfred Weirich <alfred.weirich@gmail.com>
2026-07-27 09:07:38 +0200

    Neue Reglen für die Rooms in ChatApp

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,1 @@
-RUST_LOG=warn,proxy_server=trace,proxy_server::middleware::logger=trace cargo run -p proxy_server --release
+RUST_LOG=warn,proxy_server=warn,proxy_server::middleware::logger=warn cargo run -p proxy_server --release
